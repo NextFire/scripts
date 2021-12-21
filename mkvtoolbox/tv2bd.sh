36d60e88fb3357dd634b1cb9484f67f759ae9382
NextFire <git@yuru.moe>
2021-12-22 00:28:51 +0100

    update

--- mkvtoolbox/tv2bd.sh
+++ mkvtoolbox/tv2bd.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
+set -x
 
-cd "$(dirname "$0")"
-mkdir 1subs 3out
+srcd="$1"
+rawd="$2"
+kfscript="$(dirname $0)/keyframes.py"
 
-for i in $(seq -f "%02g" 1 $(ls 2raw | wc -l))
+based='tv2bd'
+subsd="$based/subs"
+outd="$based/out"
+mkdir -p "$based" "$subsd" "$outd"
+
+nb=$(ls "$srcd" | wc -l)
+
+for i in $(seq -f "%02g" $nb)
 do
-  src=$(find 1source -regex ".*[^0-9S]${i}[^0-9].*")
-  dst=$(find 2raw -regex ".*[^0-9S]${i}[^0-9].*")
+  srcf=$(find "$srcd" -maxdepth 1 -regex ".*[^0-9S]$i[^0-9][^/]*\.mkv")
+  rawf=$(find "$rawd" -maxdepth 1 -regex ".*[^0-9S]$i[^0-9][^/]*\.mkv")
+  subf=$(find "$srcd" -maxdepth 1 -regex ".*[^0-9S]$i[^0-9][^/]*\.ass")
 
-  python3 keyframes.py --sushi --out-file "1subs/${i}.src.txt" "$src"
-  python3 keyframes.py --sushi --out-file "1subs/${i}.dst.txt" "$dst"
-  rm "$src.ffindex" "$dst.ffindex"
+  python3 "$kfscript" --sushi --out-file "$subsd/$i.src.kf" "$srcf"
+  python3 "$kfscript" --sushi --out-file "$subsd/$i.dst.kf" "$rawf"
+  rm "$srcf.ffindex" "$rawf.ffindex"
 
-  sushi --src "$src" --dst "$dst" --src-keyframes "1subs/${i}.src.txt" --dst-keyframes "1subs/${i}.dst.txt" \
-  --chapters none --max-kf-distance 5 -o "1subs/${i}.ass"
+  if [ -z "$subf" ];
+  then
+    sushi --src "$srcf" --dst "$rawf" \
+    --src-keyframes "$subsd/$i.src.kf" --dst-keyframes "$subsd/$i.dst.kf" \
+    --chapters none --max-kf-distance 5 -o "$subsd/$i.ass"
+  else
+    sushi --src "$srcf" --dst "$rawf" --script "$subf" \
+    --src-keyframes "$subsd/$i.src.kf" --dst-keyframes "$subsd/$i.dst.kf" \
+    --chapters none --max-kf-distance 5 -o "$subsd/$i.ass"
+  fi
 
-  ffmpeg -n -i "$dst" -i "1subs/${i}.ass" -i "$src" \
-  -map 0 -map 1 -map 2:t? -c copy -disposition:s:0 default -metadata:s:s:0 language=fre "3out/${i}.mkv"
+  ffmpeg -y -i "$rawf" -i "$subsd/$i.ass" -i "$srcf" \
+  -map 0:v -map 0:a -map 1 -map 2:t? -c copy \
+  -disposition:s:0 default -metadata:s:s:0 language=fre \
+  "$outd/$i.mkv"
 done
