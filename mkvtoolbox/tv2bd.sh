b683308871d039b4b538b6dbf96710c635f73eac
NextFire <git@yuru.moe>
2021-12-22 11:45:53 +0100

    update

--- mkvtoolbox/tv2bd.sh
+++ mkvtoolbox/tv2bd.sh
@@ -1,37 +1,34 @@
 #!/bin/bash
 set -x
 
+based='tv2bd'
+subsd="$based/subs"
+outd="$based/out"
+
+rm -rf "$based"
+mkdir -p "$subsd" "$outd"
+
 srcd="$1"
 rawd="$2"
 kfscript="$(dirname $0)/keyframes.py"
 
-based='tv2bd'
-subsd="$based/subs"
-outd="$based/out"
-mkdir -p "$based" "$subsd" "$outd"
+start=${3:-1}
+end=${4:-$(($start + $(ls "$srcd" | grep '.mkv$' | wc -l) - 1))}
 
-nb=$(ls "$srcd" | wc -l)
-
-for i in $(seq -f "%02g" $nb)
+for i in $(seq -f "%02g" $start $end)
 do
-  srcf=$(find "$srcd" -maxdepth 1 -regex ".*[^0-9S]$i[^0-9][^/]*\.mkv")
-  rawf=$(find "$rawd" -maxdepth 1 -regex ".*[^0-9S]$i[^0-9][^/]*\.mkv")
-  subf=$(find "$srcd" -maxdepth 1 -regex ".*[^0-9S]$i[^0-9][^/]*\.ass")
+  srcf=$(find "$srcd" -maxdepth 1 -regex ".*[^0-9S]$i[^0-9p][^/]*\.mkv")
+  rawf=$(find "$rawd" -maxdepth 1 -regex ".*[^0-9S]$i[^0-9p][^/]*\.mkv")
+  subf=$(find "$srcd" -maxdepth 1 -regex ".*[^0-9S]$i[^0-9p][^/]*\.ass")
 
-  python3 "$kfscript" --sushi --out-file "$subsd/$i.src.kf" "$srcf"
-  python3 "$kfscript" --sushi --out-file "$subsd/$i.dst.kf" "$rawf"
-  rm "$srcf.ffindex" "$rawf.ffindex"
+  python3 "$kfscript" --sushi --out-file "$subsd/$i.src.kf" "$srcf"; rm "$srcf.ffindex" &
+  python3 "$kfscript" --sushi --out-file "$subsd/$i.dst.kf" "$rawf"; rm "$rawf.ffindex" &
+  wait
 
-  if [ -z "$subf" ];
-  then
-    sushi --src "$srcf" --dst "$rawf" \
-    --src-keyframes "$subsd/$i.src.kf" --dst-keyframes "$subsd/$i.dst.kf" \
-    --chapters none --max-kf-distance 5 -o "$subsd/$i.ass"
-  else
-    sushi --src "$srcf" --dst "$rawf" --script "$subf" \
-    --src-keyframes "$subsd/$i.src.kf" --dst-keyframes "$subsd/$i.dst.kf" \
-    --chapters none --max-kf-distance 5 -o "$subsd/$i.ass"
-  fi
+  sushi --src "$srcf" --dst "$rawf" --script "$subf" \
+  --src-keyframes "$subsd/$i.src.kf" --dst-keyframes "$subsd/$i.dst.kf" \
+  --max-kf-distance 5 -o "$subsd/$i.ass"
+  rm "$subsd/$i.src.kf" "$subsd/$i.dst.kf"
 
   ffmpeg -y -i "$rawf" -i "$subsd/$i.ass" -i "$srcf" \
   -map 0:v -map 0:a -map 1 -map 2:t? -c copy \
